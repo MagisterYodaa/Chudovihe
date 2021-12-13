b37246d6e5df8ab2db1761c760e52ffeb2b25574
Антон Федотов <94544940+Amikan93@users.noreply.github.com>
2021-12-13 21:31:43 +0600

    Add files via upload

---
+++ see_weather.cpp
@@ -0,0 +1,7 @@
+#include "see_weather.h"
+
+void see_weather() {
+	std::cout << " В Зимбабве снег, в Норвегии с финиковых пальм " << std::endl;
+	std::cout << "    собирают урожай, в целом, погода в мире " << std::endl;
+	std::cout << "             такая же, как и всегда." << std::endl;
+}
---
+++ see_weather.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <windows.h>
+
+void see_weather();
+	
