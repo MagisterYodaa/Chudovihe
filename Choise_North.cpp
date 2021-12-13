68f19c7de74c623d18be6725b15584a8fe42d092
MagisterYodaa <94544814+MagisterYodaa@users.noreply.github.com>
2021-12-13 21:46:42 +0600

    Add files via upload

---
+++ Choise_North.cpp
@@ -0,0 +1,22 @@
+#include "Choise_North.h"
+
+void On_Off() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int choise = 0;
+	std::cout << "Какое устроиство вы хотите перезагрузить?" << std::endl;
+	std::cout << "1-Север" << std::endl;
+	std::cout << "2-Юг" << std::endl;
+	std::cin >> choise;
+
+	if (choise == 1) {
+		std::cout << "Перезагрузить север" << std::endl;
+	}
+
+	else if (choise == 2) {
+		std::cout << "Перезагрузить юг" << std::endl;
+	}
+	else {
+		std::cout << "В этом мире существует всего 2 стороны света!" << std::endl;
+	}
+}
