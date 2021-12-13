3b23ce1aeada424d855f23a6c72fd458e882f563
MagisterYodaa <94544814+MagisterYodaa@users.noreply.github.com>
2021-12-13 21:12:41 +0600

    Update INeedHelp.cpp

--- INeedHelp.cpp
+++ INeedHelp.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <Windows.h>
 
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int choise = 0;
+	std::cout << "Какое устроиство вы хотите перезагрузить?" << std::endl;
+	std::cout << "1-Север" << std::endl;
+	std::cout << "2-Юг" << std::endl;
+	std::cin >> choise;
+	
+	if (choise == 1) {
+		std::cout << "Перезагрузить север" << std::endl;
+	}
+
+	else if (choise == 2) {
+		std::cout << "Перезагрузить юг" << std::endl;
+	}
+	else {
+		std::cout << "Другие стороны света не поддерживаются!" << std::endl;
+	}
+
+}
