56311ed40d09a2a792e38b449b503ae5a270613b
MagisterYodaa <94544814+MagisterYodaa@users.noreply.github.com>
2022-01-24 20:20:51 +0600

    Update <>.cpp

--- <>.cpp
+++ <>.cpp
@@ -27,3 +27,40 @@ int main() {
 
 	}
 }
+
+
+наоборот 
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int randomNumber;
+	int low = 1;
+	int high = 100;
+	int mid, compare;
+	int guessNumber;
+	cout << "Ты загадал число, компьютер угадывает" << endl;
+	do {
+		mid = (low + high) / 2;
+		printf("Ваше число %d ?\n", mid);
+		cout << "1 - Число больше\n2 - Число меньше\n0 - Да";
+		cin >> compare;
+
+		if (compare == 1) {
+			low = mid + 1;
+		}
+		else if (compare == 2) {
+			high = mid + 1;
+		}
+		else if (compare == 0) {
+			printf("Ваше число %d", mid);
+		}
+	} while (compare != 0);
+	
+
+	
+}
