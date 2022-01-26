52c5657e6d9c705d793638500e99a585e03d68a8
MagisterYodaa <94544814+MagisterYodaa@users.noreply.github.com>
2022-01-26 21:05:58 +0600

    Update 10.01.cpp

--- 10.01.cpp
+++ 10.01.cpp
@@ -21,5 +21,31 @@ int main() {
 }
 
 Задание 6
-  
+  #include <iostream>
+#include <Windows.h>
+
+int main() {
+	const int size = 10;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::cout << "Массив:" << std::endl;
+	srand(4784);
+	int a[3][3];
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			a[i][j] = rand();
+			std::cout << a[i][j] << "\t";
+		}
+		std::cout << "\n\n";
+	}
+
+	int n = a[0][0] + a[0][1] + a[0][2];
+	int b = a[1][0] + a[1][1] + a[1][2];
+	int v = a[2][0] + a[2][1] + a[2][2];
+
+	std::cout << v << std::endl;
+
+}
+Задание 7
+
   
