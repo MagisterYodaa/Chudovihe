88c2ccdbae1573279e2df6728116ff1d2df0a6d9
MagisterYodaa <94544814+MagisterYodaa@users.noreply.github.com>
2022-02-02 20:19:07 +0600

    Update 02.02.22.cpp

--- 02.02.22.cpp
+++ 02.02.22.cpp
@@ -29,4 +29,58 @@ int main()
 
 Задание 2 
   
-  
+  #include <iostream>
+
+int massive(int(*a)[5]) {
+    srand(time(0));
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            a[i][j] = 30 + rand() % (60 - 30 + 1);
+        }
+    }
+    return 0;
+}
+
+int maximum(int(*a)[5]) {
+    int max = a[0][0];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (a[i][j] > max) {
+                max = a[i][j];
+            }
+        }
+    }
+    return max;
+}
+
+int minimum(int(*a)[5]) {
+    int min = a[0][0];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (a[i][j] < min) {
+                min = a[i][j];
+            }
+        }
+    }
+    return min;
+}
+
+int main()
+{
+
+    int c[5][5];
+    massive(c);
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            std::cout << c[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "Максимальное число: " << maximum(c) << std::endl;
+    std::cout << "Минимальное число: " << minimum(c) << std::endl;
+
+
+}
+
+
+Задание 3
