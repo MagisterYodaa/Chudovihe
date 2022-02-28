9c3d168625415bbe0e89c71f709fd5fe7f4845c3
MagisterYodaa <94544814+MagisterYodaa@users.noreply.github.com>
2022-02-28 18:56:15 +0600

    Create zd.cpp

---
+++ zd.cpp
@@ -0,0 +1,136 @@
+задание номер 1
+
+#include <iostream>
+
+
+
+int main() {
+	int a = 10;
+	int b = 0;
+	for (int i = 0; i <= 78; i++) {
+		b += a + i ;
+		
+		
+		std::cout << b << std::endl;
+	}
+	
+}
+
+
+
+задание номер 2
+	
+
+	
+#include <iostream>
+
+
+
+
+int main() {
+	int a = 5;
+	int b = 0;
+	for (int i = 5; i < 13; i++) {
+		a += a * i;
+
+
+		std::cout << a << std::endl;
+	}
+   }
+
+
+задание номер 3
+	
+	
+
+	
+	
+	
+	
+	
+#include <iostream>
+
+
+
+
+int main() {
+	int a = 1;
+	int b = 0;
+	for (int i = 0; i < 113; i+=3) {
+		b += a + i;
+
+
+		std::cout << b << std::endl;
+	}
+   }
+
+
+
+
+задание номер 4
+	
+	
+#include <iostream>
+
+
+
+
+int main() {
+	int a = 2;
+	float b = 0;
+	for (float i = 3; i < 11; i++) {
+		b += a / (i + i);
+
+
+		std::cout << b << std::endl;
+	}
+   }
+
+
+
+задание номер 5
+	
+	
+	
+#include <iostream>
+
+
+
+
+int main() {
+	int a = 0;
+	float b = 0;
+	for (float i = 1; i < 101; i++) {
+		b += a + i;
+
+
+		std::cout << b << std::endl;
+	}
+   }
+
+
+
+
+
+
+ЗАДАНИЕ НОМЕР 7
+	
+	
+	
+	
+#include <iostream>
+
+
+
+
+int main() {
+	int a;
+	float b = 0;
+	std::cin >> a;
+	for (float i = 1; i < 10; i++) {
+		b += (i + 1) / (a + 1);
+
+
+		std::cout << b << std::endl;
+	}
+}
