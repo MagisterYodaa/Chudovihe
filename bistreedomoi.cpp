b2163d2c9d43cc6f7e90a201a0ea46c7208822b3
MagisterYodaa <94544814+MagisterYodaa@users.noreply.github.com>
2022-02-28 19:58:35 +0600

    Update bistreedomoi.cpp

--- bistreedomoi.cpp
+++ bistreedomoi.cpp
@@ -17,5 +17,29 @@ int main(void) {
 
 	return 0;
 }
+
+
 Задание 2
-  
+#include <iostream>
+#include <Windows.h>
+
+int func(int x , int& y , int z , int h , int w) {
+	z = sqrt((x - h)*2 + (y - w)*2);
+	return z;
+
+}
+                                                                
+
+int main(void) {
+	int a{ 0 }, b{}, c{ 0 }, d{ 0 } , g{0} , j{0};
+	std::cin >> a;
+	std::cin >> c;
+	std::cin >> g;
+	std::cin >> j;
+	b = func(a, c , d , g , j);
+	std::cout << b << std::endl;
+
+	return 0;
+}
+
+Задание 3
