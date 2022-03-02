b20f052351bc32916ee92f29be9c4a97547de8c9
MagisterYodaa <94544814+MagisterYodaa@users.noreply.github.com>
2022-03-02 20:06:36 +0600

    Update 2.03.2022.cpp

--- 2.03.2022.cpp
+++ 2.03.2022.cpp
@@ -19,3 +19,27 @@ int main() {
 	std::cout << fib(10) << std::endl;
 
 }
+
+Задание 1
+	
+	#include <iostream>
+#include<cmath>
+
+int fib(int n) {
+	if (n == 0) {
+		std::cout << "Это нолик" << std::endl;
+	}
+	else {
+		return n * n * n;
+	}
+}
+
+int main() {
+	std::cout << "Hello" << std::endl;
+	std::cout << fib(5) << std::endl;
+
+}
+
+
+Задание 2
+	
