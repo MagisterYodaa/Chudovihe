c1315f611fb265feacc521f422ec7df154e2245f
MagisterYodaa <94544814+MagisterYodaa@users.noreply.github.com>
2022-04-04 20:52:55 +0600

    Create 4.04.2022.cpp

---
+++ 4.04.2022.cpp
@@ -0,0 +1,38 @@
+Первое задание 
+//struct date
+//{
+//	int day;
+//	int month;
+//	int year;
+//};
+//
+//
+//int showdate(date my, date second) {
+//	if (my.year == second.year) {
+//		if (my.month == second.month) {
+//			return (second.day - my.day);
+//		}
+//		else {
+//			return (second.day - my.day + 30);
+//		}
+//	}
+//	if (my.month == second.month) {
+//		return(second.day - my.day + 365);
+//	}
+//	else {
+//		return(second.day - my.day + 30 + 365);
+//	}
+//}
+//
+//
+//int main() {
+//	date my_date{ 15, 4, 2022 };
+//	date second_date{ 25, 4, 2022 };
+//	showdate(my_date, second_date);
+//	cout << showdate(my_date, second_date) << endl;
+//	getchar();
+//	return 0;
+//}
+
+
+второе задание
