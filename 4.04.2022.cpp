b26ff90712ba1accb0e92a8b2e63978fe7bdfaf2
MagisterYodaa <94544814+MagisterYodaa@users.noreply.github.com>
2022-04-04 21:03:05 +0600

    Update 4.04.2022.cpp

--- 4.04.2022.cpp
+++ 4.04.2022.cpp
@@ -36,3 +36,39 @@
 
 
 второе задание
+
+
+struct date
+{
+	int day;
+	int month;
+	int year;
+};
+
+
+int showdate(date my, date second) {
+	if (my.year == second.year) {
+		if (my.month == second.month) {
+			return (second.day - my.day);
+		}
+		else {
+			return (second.day - my.day + 60);
+		}
+	}
+	if (my.month == second.month) {
+		return(second.day - my.day + 3600);
+	}
+	else {
+		return(second.day - my.day + 60 + 3600);
+	}
+}
+
+
+int main() {
+	date my_date{ 30, 15, 3 };
+	date second_date{ 25, 15, 3 };
+	showdate(my_date, second_date);
+	cout << showdate(my_date, second_date) << endl;
+	getchar();
+	return 0;
+}
